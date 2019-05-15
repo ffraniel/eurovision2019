c32f318f19dd2f53dc47618ce0054197e68ab22d
ffraniel <francis.whitehead@googlemail.com>
2019-05-15 15:45:50 +0100

    added title, added fade in on intro and meta data

--- src/Intro.js
+++ src/Intro.js
@@ -5,7 +5,10 @@ import Logo from './logo.svg';
 const Intro = (props) => {
   return (
     <section className="intro">
-      <h1>Eurovision 2019 Lyrics Game</h1>
+      <section className="intro-titles">
+        <h1 className="intro-title">Nul Points!</h1>
+        <h1 className="intro-sub-title">Eurovision 2019</h1>
+      </section>
       <p>Guess the country from the lyrics</p>
       <img className="logo" src={Logo} alt="logo" />
       <button onClick={()=>{props.startGame()}} className="game-button center-start">Start</button>
